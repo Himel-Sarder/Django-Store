06015fde0769fc30941ac4d8c7885e40bdada16d
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-07-31 00:51:22 +0600

    Add files via upload

---
+++ contact app using crispy forms/contact_form/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'required': True}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'required': True}),
+        }
