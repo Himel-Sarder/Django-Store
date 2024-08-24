c8eb0427ed027641f05a3c8754b52347a2dce216
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-08-24 22:38:14 +0600

    Add files via upload

--- Initiate Form Field Value Form Methods/App/forms.py
+++ Initiate Form Field Value Form Methods/App/forms.py
@@ -4,3 +4,17 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].initial = 'initial_name'  # Set default initial value for name field
+        self.fields['email'].initial = 'initial_email'  # Set default initial value for email field
+
+    def clean_name(self):
+        value = self.cleaned_data.get('name')  # Get the cleaned data for the 'name' field
+        num_of_w = value.split(' ')  # Split the name into words based on spaces
+        
+        if len(num_of_w) > 3:  # Check if there are more than 3 words
+            self.add_error('name', 'Name can have a maximum of 3 words')  # Add an error if validation fails
+        else:
+            return value  # Return the value if validation passes
--- Initiate Form Field Value Form Methods/App/views.py
+++ Initiate Form Field Value Form Methods/App/views.py
@@ -2,16 +2,8 @@ from django.shortcuts import render
 from .forms import ContactForm
 
 def contact_view(request):
-
-    # Use of initial in django
-
-    initials = {
-        'name': 'My name is ',
-        'email': '@gmail.com',
-        'message': 'Hello'
-    }
     if request.method == 'POST':
-        form = ContactForm(request.POST, initial=initials)
+        form = ContactForm(request.POST)
         if form.is_valid():
             # Process the form data
             name = form.cleaned_data['name']
@@ -20,6 +12,6 @@ def contact_view(request):
             # Here, you might save the data, send an email, etc.
             return render(request, 'contact_success.html', {'form': form})
     else:
-        form = ContactForm(initial=initials)
+        form = ContactForm()
 
     return render(request, 'contact.html', {'form': form})
